53d42eed89be4ac10de7493fef796ac0e77fe768
David <David.Pacold@Gmail.com>
2022-09-15 21:18:50 -0400

    Week 2 Homework
    Week 2 homework update

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ addEventListener("fetch", event => {
     event.respondWith(handleRequest(event.request))
   })
   
-  export default {
-   fetch(request) {
-     console.log(request.url)
-     if(request.method== "POST") {
-       return new Response('Hello worker!', {
-         headers: {
-           'content-type': 'application/json',
-         },
-       });
-     }
-     else {
-       return new Response('Sorry, wrong method type!', {
-         headers: {
-           'content-type': 'text/plain',
-         },
-       });
-     }
-   },
-  };
+  const responseMap = {
+      0: 'hello',
+      1: 'welcome',
+      2: 'goodbye',
+      3: 'cheers',
+    };
+  
+  
+  function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+
+  const randomnumber= getRandomInt(4)
+  console.log(randomnumber)
+  
+  async function handleRequest(request) {
+    if (randomnumber != null && randomnumber in responseMap) {
+    const tableresponse = responseMap[randomnumber];
+    return new Response(tableresponse)
+    }
+    else {
+          return fetch(request);
+        }
+      }
