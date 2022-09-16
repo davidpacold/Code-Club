f0bf67b1ca9cbd6bb79adca53d00ed5beb05315f
David <David.Pacold@Gmail.com>
2022-09-15 21:06:42 -0400

    week 1 homework changes
    Update Index.js for week 1 homework

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
-};
+addEventListener("fetch", event => {
+    event.respondWith(handleRequest(event.request))
+  })
+  
+  export default {
+   fetch(request) {
+     console.log(request.url)
+     if(request.method== "POST") {
+       return new Response('Hello worker!', {
+         headers: {
+           'content-type': 'application/json',
+         },
+       });
+     }
+     else {
+       return new Response('Sorry, wrong method type!', {
+         headers: {
+           'content-type': 'text/plain',
+         },
+       });
+     }
+   },
+  };
